0fe46c3a4e27620419251ef20056293fd2d44a40
bdot100 <bidemiokunade@gmail.com>
2023-10-07 22:16:31 +0100

    Create a  new user route

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,61 +1,69 @@
-import sha1 from 'sha1';
-import { ObjectID } from 'mongodb';
 import Queue from 'bull';
-import dbClient from '../utils/db';
-import redisClient from '../utils/redis';
+import { ObjectID } from 'mongodb';
+import sha1 from 'sha1';
+
+const RedisClient = require('../utils/redis');
+const DBClient = require('../utils/db');
 
 const userQueue = new Queue('userQueue', 'redis://127.0.0.1:6379');
 
 class UsersController {
-  static postNew(request, response) {
-    const { email } = request.body;
-    const { password } = request.body;
+  static postNew(req, res) {
+    try {
+      const { email, password } = req.body;
 
-    if (!email) {
-      response.status(400).json({ error: 'Missing email' });
-      return;
-    }
-    if (!password) {
-      response.status(400).json({ error: 'Missing password' });
-      return;
-    }
-
-    const users = dbClient.db.collection('users');
-    users.findOne({ email }, (err, user) => {
-      if (user) {
-        response.status(400).json({ error: 'Already exist' });
-      } else {
-        const hashedPassword = sha1(password);
-        users.insertOne(
-          {
-            email,
-            password: hashedPassword,
-          },
-        ).then((result) => {
-          response.status(201).json({ id: result.insertedId, email });
-          userQueue.add({ userId: result.insertedId });
-        }).catch((error) => console.log(error));
+      // Check if email and password are provided
+      if (!email) {
+        res.status(400).json({ error: 'Missing email' });
+        return;
       }
-    });
+
+      if (!password) {
+        res.status(400).json({ error: 'Missing password' });
+        return;
+      }
+
+      // Check if the email already exists in the database
+      const users = DBClient.db.collection('users');
+      users.findOne({ email }, (err, user) => {
+        if (user) {
+          res.status(400).json({ error: 'Already exist' });
+        } else {
+          const hashedPassword = sha1(password);
+          users.insertOne(
+            {
+              email,
+              password: hashedPassword,
+            },
+          ).then((result) => {
+            res.status(201).json({ id: result.insertedId, email });
+            userQueue.add({ userId: result.insertedId });
+          }).catch((error) => console.log(error));
+        }
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
   }
 
-  static async getMe(request, response) {
-    const token = request.header('X-Token');
+  static async getMe(req, res) {
+    const token = req.header('X-Token');
     const key = `auth_${token}`;
-    const userId = await redisClient.get(key);
+    const userId = await RedisClient.get(key);
     if (userId) {
-      const users = dbClient.db.collection('users');
+      const users = DBClient.db.collection('users');
       const idObject = new ObjectID(userId);
       users.findOne({ _id: idObject }, (err, user) => {
         if (user) {
-          response.status(200).json({ id: userId, email: user.email });
+          res.status(200).json({ id: userId, email: user.email });
         } else {
-          response.status(401).json({ error: 'Unauthorized' });
+          res.status(401).json({ error: 'Unauthorized' });
         }
       });
     } else {
       console.log('Hupatikani!');
-      response.status(401).json({ error: 'Unauthorized' });
+      res.status(401).json({ error: 'Unauthorized' });
     }
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,35 @@
-const express = require('express');
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+// // eslint-disable-next-line import/no-unresolved, import/extensions
+// import AuthController from '../controllers/AuthController';
+// // eslint-disable-next-line import/no-unresolved, import/extensions
+// import FilesController from '../controllers/FilesController';
 
-const router = express.Router();
-const AppController = require('../controllers/AppController');
-const UsersController = require('../controllers/UsersController');
+const router = Router();
 
-// Define the /status and /stats routes
 router.get('/status', AppController.getStatus);
+
 router.get('/stats', AppController.getStats);
 
-router.get('/users', UsersController.postNew);
+router.post('/users', UsersController.postNew);
+
+// router.get('/connect', AuthController.getConnect);
+
+// router.get('/disconnect', AuthController.getDisconnect);
+
+// router.get('/users/me', UsersController.getMe);
+
+// router.post('/files', FilesController.postUpload);
+
+// router.get('/files/:id', FilesController.getShow);
+
+// router.get('/files', FilesController.getIndex);
+
+// router.put('/files/:id/publish', FilesController.putPublish);
+
+// router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
+// router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
