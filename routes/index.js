f1328025785b0d9d4078de0ea04a28ab25608320
bdot100 <bidemiokunade@gmail.com>
2023-10-08 22:13:50 +0100

    Authenticate a user

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
-// // eslint-disable-next-line import/no-unresolved, import/extensions
-// import AuthController from '../controllers/AuthController';
+import AuthController from '../controllers/AuthController';
 // // eslint-disable-next-line import/no-unresolved, import/extensions
 // import FilesController from '../controllers/FilesController';
 
@@ -14,11 +13,11 @@ router.get('/stats', AppController.getStats);
 
 router.post('/users', UsersController.postNew);
 
-// router.get('/connect', AuthController.getConnect);
+router.get('/connect', AuthController.getConnect);
 
-// router.get('/disconnect', AuthController.getDisconnect);
+router.get('/disconnect', AuthController.getDisconnect);
 
-// router.get('/users/me', UsersController.getMe);
+router.get('/users/me', UsersController.getMe);
 
 // router.post('/files', FilesController.postUpload);
 
